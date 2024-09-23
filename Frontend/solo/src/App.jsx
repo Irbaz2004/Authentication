f9aeedf13a5afda090a5a4a9d9eee6731d157670
Irbaz2004 <irbazahmeds20@gmail.com>
2024-09-23 21:10:27 +0530

    change

--- Frontend/solo/src/App.jsx
+++ Frontend/solo/src/App.jsx
@@ -1,6 +1,5 @@
 import Signup from './Components/Signup'
 import Login from './Components/Login'
-import Home from './Components/Home'
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
 
@@ -12,8 +11,7 @@ function App() {
       
     <Route exact path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route exact path="/home" element={<Home />} /> 
-    </Routes>
+\    </Routes>
     </>
   )
 }
